282f86887e7e877941754ca11b93c5f40e887584
Vlad Volkov <from5jforever@gmail.com>
2021-04-09 18:18:55 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import './App.css'
+
 const App = () => {
     return (
         <div>
-            Hello!
+            <h1>TASKS</h1>
+            <button style={{color: 'blue'}}>Add a task</button>
+            <ul>
+                <li>Task 1</li>
+                <li>Task 2</li>
+                <li>Task 3</li>
+            </ul>
+            <button style={{color: 'gray'}}>Completed</button>
         </div>
     )
 }
