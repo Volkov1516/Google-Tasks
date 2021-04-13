1a041f5d94743b2dfc24b1c33c46f43d54de147d
Vlad Volkov <from5jforever@gmail.com>
2021-04-13 14:39:39 +0300

    Изменил дизайн

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
+import { useState } from 'react' 
 import './App.css'
 
 const App = () => {
+    const [inputValue, setInputValue] = useState('')
+    const [comments, setComment] = useState([])
+
+    const addComment = () => {
+        const newComment = inputValue
+        setComment([newComment, ...comments])
+        setInputValue('')   
+        console.log(comments)
+    }
+
     return (
-        <div>
-            <h1>TASKS</h1>
-            <button style={{color: 'blue'}}>Add a task</button>
-            <ul>
-                <li>Task 1</li>
-                <li>Task 2</li>
-                <li>Task 3</li>
-            </ul>
-            <button style={{color: 'gray'}}>Completed</button>
+        <div className='container'>
+            <h1 className='list-name' >TASKS</h1>
+            <input className='input' value={inputValue} onChange={e => setInputValue(e.target.value)} />
+            <button className='add-task-btn' onClick={addComment} >Add a task</button>
+            <div className='list'>
+                <ul>
+                    {comments.map(comment=> <li>{comment}</li>)}
+                </ul>
+            </div>
+            <button className='show-completed-btn' >Completed</button>
         </div>
     )
 }
