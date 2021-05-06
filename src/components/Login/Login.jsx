53f80639bb6aa62aee8533298e6b130cb9c248cc
Vlad Volkov <from5jforever@gmail.com>
2021-05-06 09:59:48 +0300

    6.5.21

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,20 @@
+import { useForm } from 'react-hook-form'
+
 import css from './Login.module.jsx'
 
 const Login = () => {
+    const {register, handleSubmit, erros} = useForm()
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
-        <form>
-            
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>Email</label>
+            <input type="text" name="email" ref={register} />
+            <label>Password</label>
+            <input type="password" name="password" ref={register} />
+            <button type="submit" >Login</button>
         </form>
     )
 }
