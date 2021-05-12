69ec0c877077eda5a45b94e41af8eb59ef5d2664
Vlad Volkov <from5jforever@gmail.com>
2021-05-12 17:01:28 +0300

    12.5.21

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,25 +2,27 @@ import { useState } from 'react'
 import DB from '../db.json'
 
 const List = () => {
-    const [tasks, setTask] = useState( DB.tasks )
+    const [tasks, setTask] = useState(DB.tasks)
     const [inputValue, setInputValue] = useState('')
     const addTask = () => {
         const id = tasks.length
-        const newTask = {id, task: inputValue}
+        const newTask = { id, task: inputValue }
         setTask([newTask, ...tasks])
-        setInputValue('') 
+        setInputValue('')
     }
 
     return (
-        <div>
-            <input className='input' value={inputValue} onChange={e => setInputValue(e.target.value)} />
-            <button className='add-task-btn' onClick={addTask} >Add</button>
-            <div className='list'>
+        <div className="container__list">
+            <div className='list__control-panel'>
+                <input className='control-panel__input' value={inputValue} onChange={e => setInputValue(e.target.value)} />
+                <button className='control-panel__add-task-btn' onClick={addTask} >Add</button>
+            </div>
+            <div className='list__lists-area'>
                 <ul>
                     {tasks.map((i, index) =>
                         <li key={i.id}>
-                            <input type="text"  value={i.task} />
-                            <button onClick={() => tasks.splice(index, 1)} >delete</button>
+                            <button className='lists-area__remove-btn' onClick={() => tasks.splice(index, 1)} ></button>
+                            <input className='lists-area__input' type="text" value={i.task} />
                         </li>
                     )}
                 </ul>
