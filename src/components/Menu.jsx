25d59dadf7021a9fba57ee7a75328935c9e9f89b
Vlad Volkov <from5jforever@gmail.com>
2021-05-15 18:56:27 +0300

    15.5.21

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,19 +7,10 @@ const Menu = () => {
         setIsVisible(!isVisible)
     }
 
-    const showList = (id, tasks) => {
-        console.log(tasks)
-    } 
-
     return (
         <div className='container__menu'>
             <button className='menu-btn' onClick={showMenu} >{DB.list.name}</button>
             <ul className='menu' style={isVisible ? { 'display': 'block' } : { 'display': 'none' }}>
-                {DB.menu.map(i => 
-                    <li  key={i.id} onClick={() => showList(i.id, i.tasks)}>
-                        {i.name}
-                    </li>
-                )}
                 <li>Книги</li>
                 <li className='menu__create-new' >Создать новый список</li>
                 <li className='menu__user' >mymail@mail.com</li>
