216797915f7c38d94187ce547ce22835400e7734
Vlad Volkov <from5jforever@gmail.com>
2021-05-15 18:38:27 +0300

    15.5.21

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,25 +1,25 @@
 import { useState } from 'react'
 import DB from '../db.json'
-/**
- * Редактировать JSON
- * Отобразить список листов в меню
- * В зависимости от выбранного листа, рендерить соответствующий список задач
- * По нажатию на кнопку конкретного элемента(имени списка) запускается функция 
- * Если id из события клика равен одному из id имени списка, то нужно рендерить массив списков этого объекта
- */
+
 const Menu = () => {
-    const [lists, setLists] = useState(DB.list)
     const [isVisible, setIsVisible] = useState(false)
     const showMenu = () => {
         setIsVisible(!isVisible)
     }
 
+    const showList = (id, tasks) => {
+        console.log(tasks)
+    } 
+
     return (
         <div className='container__menu'>
             <button className='menu-btn' onClick={showMenu} >{DB.list.name}</button>
             <ul className='menu' style={isVisible ? { 'display': 'block' } : { 'display': 'none' }}>
-                <li>Напоминания</li>
-                <li>Проекты</li>
+                {DB.menu.map(i => 
+                    <li  key={i.id} onClick={() => showList(i.id, i.tasks)}>
+                        {i.name}
+                    </li>
+                )}
                 <li>Книги</li>
                 <li className='menu__create-new' >Создать новый список</li>
                 <li className='menu__user' >mymail@mail.com</li>
