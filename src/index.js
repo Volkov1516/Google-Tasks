f5ad5db034537ffa67170b12723aebd892c6e2c0
Vlad Volkov <from5jforever@gmail.com>
2021-05-22 18:38:51 +0300

    22.05.21

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ import List from './components/List'
 const App = (props) => {
     const isLoggedIn = props.isLoggedIn
     const [database, setDatabase] = useState(DB)
+    const [a, setA] = useState(DB.acd.map(i => {if(i.userId === 1) return i.tasks}))
+    console.log(a)
 
     if (isLoggedIn) {
         return (
             <div className='container'>
+                {a[0].map(i => <span>{i.text}</span>)}
                 <List tasks={database.list.tasks} completed={database.list.completed} />
             </div>
         )
