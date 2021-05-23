c8298d534b8c1f59b119a924f91353b575fb256f
Vlad Volkov <from5jforever@gmail.com>
2021-05-23 17:17:21 +0300

    23.05.21

--- src/index.js
+++ src/index.js
@@ -10,14 +10,27 @@ import List from './components/List'
 const App = (props) => {
     const isLoggedIn = props.isLoggedIn
     const [database, setDatabase] = useState(DB)
-    const [a, setA] = useState(DB.acd.map(i => {if(i.userId === 1) return i.tasks}))
-    console.log(a)
+    const [data, setData] = useState(DB.acd.map(i => { if (i.userId === 2) return i }))
+    console.log(data)
+    const [a, setA] = useState(DB.acd.map(i => { if (i.userId === 1) return i.tasks }))
+    console.log(a[0])
 
     if (isLoggedIn) {
         return (
             <div className='container'>
                 {a[0].map(i => <span>{i.text}</span>)}
                 <List tasks={database.list.tasks} completed={database.list.completed} />
+
+                <div>
+                    <span>from5jforever@gmail.com</span>
+                    <h1>ЖУРНАЛ</h1>
+                    <button>ДОБАВИТЬ</button>
+                    <div>
+                        SCROLL
+                    </div>
+                </div>
+
+
             </div>
         )
     }
