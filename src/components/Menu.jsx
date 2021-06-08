35e83ead847d7640d0723c381b6b2e3d3e7eb5f2
Vlad Volkov <from5jforever@gmail.com>
2021-06-08 18:42:25 +0300

    080621

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react'
 
-function Menu({data, getId, creacteList}) {
+function Menu({data, getId, creacteList, activeListName, removeList}) {
     const [isVisible, setIsVisible] = useState(false)
     const showLists = () => {
         setIsVisible(!isVisible)
@@ -8,11 +8,14 @@ function Menu({data, getId, creacteList}) {
 
     return (
         <div>
-            <button style={{display: 'block'}} onClick={showLists}>Select List</button>
+            <button style={{display: 'block'}} onClick={showLists}>{activeListName}</button>
             <div style={isVisible ?  ({display: 'block'}) : ({display: 'none'})}>
                 <ul>
-                    {data.map(i => <li onClick={() => getId(i.id)}>{i.id}</li>)}
-                    <button onClick={() => creacteList()}>Create a new list</button>
+                    {data.map(i => <li onClick={() => getId(i.id, i.name)}>{i.name}
+                        <button>Edit</button>
+                        <button onClick={() => removeList(i.id)}>Remove</button>
+                    </li>)}
+                    <button onClick={() => creacteList()}>Creacte a new</button>
                 </ul>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -4,25 +4,38 @@ import { BrowserRouter } from 'react-router-dom'
 
 import List from './components/List'
 import Menu from './components/Menu'
-
+/**
+ * 3. Полная реализация кнопки добавления списков
+ * 4. Функция переименования списка 
+ * 5. Упаковка данных в JSON и подключение JSONserver
+ * 6. useReduser?
+ * 7. TypeScript?
+ * 8. Добавление стилей(Изучить соответствующие библиотеки)
+ */
 const App = () => {
     const [data, setData] = useState([
         {
             id: 1,
+            name: "(id: 1) Мои задачи",
             tasks: [
                 {
                     id: 1,
-                    task: "ID 1"
+                    task: "(id: 1) Завершить проект"
+                },
+                {
+                    id: 2,
+                    task: "(id: 1) Купить новый дневник"
                 }
             ],
             completed: []
         },
         {
             id: 2,
+            name: "(id: 2) Покупки",
             tasks: [
                 {
                     id: 2,
-                    task: "ID 2"
+                    task: "(id: 2)Купить одежду"
                 }
             ],
             completed: []
@@ -30,13 +43,16 @@ const App = () => {
     ])
 
     const [activeList, setActiveList] = useState(1)
-    const setId = (id) => {
+    const [activeListName, setActiveListName] = useState('Выберите список')
+    const setId = (id, name) => {
         setActiveList(id)
+        setActiveListName(name)
     }
 
     const creacteList = () => {
         const updatedList = {
             id: data.length + 1,
+            name: "Новый список",
             tasks: [
                 {
                     id: 3,
@@ -48,9 +64,15 @@ const App = () => {
         setData([...data, updatedList])
     }
 
+    const removeList = (id) => {
+        const updatedList = [...data].filter(i => i.id != id)
+        setData(updatedList)
+    }
+
+    
     return (
         <div className='container'>
-            <Menu data={data} getId={setId} creacteList={creacteList}/>
+            <Menu data={data} getId={setId} creacteList={creacteList} activeListName={activeListName} removeList={removeList} />
             {data.map((i) => {
                 if(i.id === activeList)
                     return <List tasks={i.tasks} completed={i.completed}/>
